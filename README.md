e001fed4da2b7291581a543c7f3b6a9a63eaf2f0
Elliot Penson <elliot.penson@it.com>
2018-10-15 20:18:55 -0400

    Allow query combination with logical operators

--- README.md
+++ README.md
@@ -8,11 +8,11 @@ A high-level library for querying Solr with Python. Built on the lower-level Pys
 from pysolr import Solr
 from solr_dsl import Search
 
-solr = Solr("http://localhost:8983/solr/test")
-search = Search(solr) \
-    .require("doc_type", "solution") \
-    .require("domain", "example1", "example2")
-    .with_range("date", "2018-01-01T00:00:00Z", "now")
+solr = Solr('http://localhost:8983/solr/test')
+
+query = (Field('doc_type', 'solution') &
+         Range("date", "2018-01-01T00:00:00Z", "now"))
+search = Search(solr, query)
 
 for document in search.scan():
     ...
--- solr_dsl/search.py
+++ solr_dsl/search.py
@@ -3,30 +3,10 @@ from .cursor import Cursor
 
 class Search():
 
-    def __init__(self, solr):
+    def __init__(self, solr, query="*:*"):
         self.solr = solr
-        self.queries = []
-
-    def require(self, key, *values):
-        body = " ".join(f"{key}:\"{value}\"" for value in values)
-        query = f"+({body})"
-        self.queries.append(query)
-        return self
-
-    def prohibit(self, key, *values):
-        body = " ".join(f"{key}:\"{value}\"" for value in values)
-        query = f"-({body})"
-        self.queries.append(query)
-        return self
-
-    def with_range(self, key, start="*", stop="*"):
-        self.queries.append(f"+({key}:[{start} TO {stop}])")
-        return self
+        self.query = query
 
     def scan(self):
         """Iterate over documents that match the search."""
-        return Cursor(self.solr, self.build())
-
-    def build(self):
-        """Construct the query."""
-        return " ".join(self.queries) or "*:*"
+        return Cursor(self.solr, self.query)
