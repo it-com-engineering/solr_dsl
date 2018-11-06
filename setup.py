0e1225030c3d19ce5e8f2769449f129f4dfc9e89
Elliot Penson <elliot.penson@it.com>
2018-11-06 00:00:55 -0500

    Release 0.0.11

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setuptools.setup(
     name="solr-dsl",
-    version="0.0.3",
+    version="0.0.11",
     author="Elliot Penson",
     author_email="elliotpenson@gmail.com",
     description="Python client for Solr",
